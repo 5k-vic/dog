d7f59c99c05f7d90ed880fc576f02a297501f054
5kvic <joao-victor.oliveira@patelhs.org>
2020-10-14 17:59:00 +0000

    none

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args){
+  Scanner scan = new Scanner(System.in);
+  
 
+  System.out.println("how old is your dog?");
+  int dogsAge = scan.nextInt();
 
+  System.out.println("my dog is " + dogsAge);
 
+  System.out.println("What color is yor dog?");
+  String dogColor = scan.next();
 
+  System.out.println("my dog's color is " + dogColor);
 
-    
   }
 }
