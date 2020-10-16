668eeae11bc46bc63d25a4fe99128882731399c8
5kvic <joao-victor.oliveira@patelhs.org>
2020-10-16 15:58:09 +0000

    alot

--- Main.java
+++ Main.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args){
-  Scanner scan = new Scanner(System.in);
-  
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-  System.out.println("how old is your dog?");
-  int dogsAge = scan.nextInt();
+    System.out.println("how old is your dog?");
+    int dogsAge = scan.nextInt();
 
-  System.out.println("my dog is " + dogsAge);
+    System.out.println("my dog is " + dogsAge);
 
-  System.out.println("What color is yor dog?");
-  String dogColor = scan.next();
+    System.out.println("What color is yor dog?");
+    String dogColor = scan.next();
 
-  System.out.println("my dog's color is " + dogColor);
+    System.out.println("my dog's color is " + dogColor);
+
+    System.out.println("do you want puppies?");
+    String noPuppies = scan.next();
+    System.out.println("true or false");
+    boolean puppers = scan.nextBoolean();
+    
+    if (puppers) {
+      System.out.println("how many?");
+      int dogs = scan.nextInt();
+
+    }else {
+     System.out.println("why not?");
+    }
+
+    
 
   }
 }
