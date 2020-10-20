48a0b4cb04bef7e16ae5b5f046f572d6a7b84ef6
5kvic <joao-victor.oliveira@patelhs.org>
2020-10-20 14:38:18 +0000

    add loop

--- Main.java
+++ Main.java
@@ -5,28 +5,40 @@ class Main {
     Scanner scan = new Scanner(System.in);
 
     System.out.println("how old is your dog?");
+    //start of scanner with int
     int dogsAge = scan.nextInt();
 
     System.out.println("my dog is " + dogsAge);
-
+ 
     System.out.println("What color is yor dog?");
+   //start of scanner with string
     String dogColor = scan.next();
 
     System.out.println("my dog's color is " + dogColor);
 
     System.out.println("do you want puppies?");
-    String noPuppies = scan.next();
     System.out.println("true or false");
+   //start of scanner with boolean
     boolean puppers = scan.nextBoolean();
-    
+   //start of if/else statements 
     if (puppers) {
       System.out.println("how many?");
-      int dogs = scan.nextInt();
+      int puppies = scan.nextInt();
+      if (puppies > 5){
+        System.out.println("so manyyy");
+      }
 
     }else {
      System.out.println("why not?");
+    String noPuppies = scan.next();
     }
+    int x = 5;
+    while (x > 0)
+    {
+     x++;
+     System.out.prinln( x + "did you feed the dog");
 
+    }
     
 
   }
